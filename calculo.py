7de016ec1b3865880f6d1d781da4d6e3aa2c8661
Mateus Glória <129634511+Mateus-gloria@users.noreply.github.com>
2023-08-27 19:39:00 -0300

    Add files via upload

---
+++ calculo.py
@@ -0,0 +1,33 @@
+'soma'
+print(5+3)
+
+'subtração'
+print(5-2)
+
+'soma + subtração'
+print(5+9-6)
+
+'multiplicação'
+print(5*2)
+
+'divisão'
+print(9/3)
+
+'Exponenciação'
+print(2**3)
+
+' Resto da divisão inteira'
+print(10%3)
+
+print(16%7)
+
+print(63%8)
+
+'Exercício 2.1 Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.'
+
+print(10+20*30)
+print(42/30)
+print(94+2) * 6 - 1
+
+'Exercício 2.2 Digite a seguinte expressão no interpretador:'
+print(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
